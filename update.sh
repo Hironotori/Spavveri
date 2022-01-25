745849ae9056912998d6048b28fe7c8372411d41
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-25 22:20:48 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 cd
 rm -rf bombertest1
-git clone https://github.com/Hironotori/bombertest1
+git clone https://github.com/Hironotori/Bomber
 cd
 rm -rf bombertest1
-git clone https://github.com/Hironotori/bombertest1
+git clone https://github.com/Hironotori/Bomber
 cd
 cd bombertest1
 python3 Spavveri.py
